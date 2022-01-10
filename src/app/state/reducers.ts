fcff98ae3f42772bcb42f25852b1850b5db79966
Dylan Lamont <dylanlamont3840@gmail.com>
2022-01-10 17:42:18 +1100

    Implements immer for reducers

--- src/app/state/reducers.ts
+++ src/app/state/reducers.ts
@@ -1,28 +1,18 @@
-import { clone } from "../utils/clone";
 import { Action, GlobalState } from "./types";
+import produce from "immer";
 
 export const reducers = (state: GlobalState, action: Action): GlobalState => {
   const { type, playerIndex = 0 } = action;
 
   switch (type) {
     case "lp/increment": {
-      const _state = clone(state ?? {});
-      let playerLp = _state.players[playerIndex].lp;
-      if (playerLp < 99) _state.players[playerIndex].lp += 1;
-      return _state;
-
-      //   let lastLog = { ..._lpLog.slice(-1)[0] };
-      //   if (lastLog.lp < 99) lastLog.lp += 1;
-      //   return { ...state, lpLog: [...lpLog, lastLog] };
+      return produce(state, (draftState) => {
+        if (draftState) {
+          const playerLp = draftState?.players[playerIndex].lp;
+          if (playerLp < 99) draftState.players[playerIndex].lp += 1;
+        }
+      });
     }
-
-    // case "lp/decrement": {
-    //   return state;
-    //   //   const _lpLog = clone(state.lpLog);
-    //   //   let lastLog = { ..._lpLog.slice(-1)[0] };
-    //   //   if (lastLog.lp > 0) lastLog.lp -= 1;
-    //   //   return { ...state, lpLog: [...lpLog, lastLog] };
-    // }
 
     default:
       throw new Error(`Unhandled action type: ${type}`);

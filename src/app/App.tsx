13987729ac252fa4f708082204cc8c083e69de3c
Dylan Lamont <38599829+didley@users.noreply.github.com>
2022-04-21 19:22:05 +1000

    Implements smaller tracker view for larger screens

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@ import { PlayerCard } from "./components/PlayerCard";
 
 function App() {
   return (
-    <div className="h-screen w-screen grid select-none bg-black">
-      <PlayerCard playerIndex={1} />
-      <TitleBar />
-      <PlayerCard playerIndex={0} />
+    <div className="grid md:bg-slate-50 h-screen w-screen">
+      <div className="bg-black grid place-self-center md:h-3/4 h-screen md:w-96 w-screen select-none md:place-self-center md:shadow-xl">
+        <PlayerCard playerIndex={1} />
+        <TitleBar />
+        <PlayerCard playerIndex={0} />
+      </div>
     </div>
   );
 }
